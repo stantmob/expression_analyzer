21eabbf02ef24d23542cc1868d0650780202d9d5
Eric C. Leão <ericcleao@gmail.com>
2019-07-15 11:00:24 -0300

    Remove unecessary spaces at spec file
    Signed-off-by: Eric C. Leão <ericcleao@gmail.com>

--- spec/expression_analyzer_spec.rb
+++ spec/expression_analyzer_spec.rb
@@ -1,28 +1,26 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require './expression_analyzer'
 
 describe ExpressionAnalyzer do
-
   subject { described_class.new(input) }
-  let(:input) {  }
+  let(:input) {}
   let(:result) { subject.call }
 
-
   context 'when pass a expression 3 * (2 + 5)' do
-
     let(:input) { '3 * (2 + 5)' }
 
     it 'should return 21' do
-      expect(result).to eq 21 
+      expect(result).to eq 21
     end
   end
 
   context 'when pass a expression (2 + 5) * 3 ' do
-
     let(:input) { '(2 + 5) * 3' }
 
     it 'should return 21' do
-      expect(result).to eq 21 
+      expect(result).to eq 21
     end
   end
 
@@ -30,7 +28,7 @@ describe ExpressionAnalyzer do
     let(:input) { '(2 - 5) * 3' }
 
     it 'should return -9' do
-      expect(result).to eq -9 
+      expect(result).to eq -9
     end
   end
 
@@ -38,7 +36,7 @@ describe ExpressionAnalyzer do
     let(:input) { '3 * (2 - 5)' }
 
     it 'should return -9' do
-      expect(result).to eq -9 
+      expect(result).to eq -9
     end
   end
 
@@ -46,7 +44,7 @@ describe ExpressionAnalyzer do
     let(:input) { '3 / (2 - 5)' }
 
     it 'should return -1' do
-      expect(result).to eq -1 
+      expect(result).to eq -1
     end
   end
 
@@ -54,7 +52,7 @@ describe ExpressionAnalyzer do
     let(:input) { '30 / (2 * 5)' }
 
     it 'should return 3' do
-      expect(result).to eq 3 
+      expect(result).to eq 3
     end
   end
 
@@ -62,7 +60,7 @@ describe ExpressionAnalyzer do
     let(:input) { '[30 / (2 * 5)] +5' }
 
     it 'should return 8' do
-      expect(result).to eq 8 
+      expect(result).to eq 8
     end
   end
 end
