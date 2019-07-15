636647ef3b64f2a0c3fc8a1928c5d700858d109a
Eric C. Leão <ericcleao@gmail.com>
2019-07-15 10:57:26 -0300

    Refactor analyzer and spec
    - Fix Robocop sugestions
    - Implement service object pattern
    
    Signed-off-by: Eric C. Leão <ericcleao@gmail.com>

--- expression_analyzer.rb
+++ expression_analyzer.rb
@@ -1,10 +1,10 @@
 class ExpressionAnalyzer
-    
-  def expression_dudao(expression_string)
-    expression_string = expression_string.delete(' ')
-    
-#(\d\s*\*)+ 3*
-    evaluate(expression_string)
+  def initialize(expression)
+    @expression = expression.delete(' ')
+  end
+
+  def call
+    evaluate(@expression)
   end
 
   def evaluate(expression)
@@ -14,37 +14,32 @@ class ExpressionAnalyzer
     elsif expression.index('(')
       split = start_evaluation(expression, '(', ')')
       evaluate(split[0]).send(split[1], evaluate(split[2]))
-    elsif index = (expression.index('+') || expression.index('-'))
+    elsif (index = (expression.index('+') || expression.index('-')))
       split = slice_at(expression, index)
       evaluate(split[0]).send(split[1], evaluate(split[2]))
-    elsif index = (expression.index('*') || expression.index('/'))
+    elsif (index = (expression.index('*') || expression.index('/')))
       split = slice_at(expression, index)
       evaluate(split[0]).send(split[1], evaluate(split[2]))
-    else 
+    else
       expression.to_i
     end
   end
 
   def start_evaluation(expression, symbol, closing_symbol)
     index = expression.index(symbol)
-    
-    split = slice_at(expression, index)
 
     closing_parenthesis_index = expression.index(closing_symbol)
     expression.slice!(closing_parenthesis_index)
     expression.slice!(index)
-    split = slice_at(expression, index-1)
+    split = slice_at(expression, index - 1)
 
-    if index == 0
-      split = slice_at(expression, closing_parenthesis_index-1)
-    end
-
+    split = slice_at(expression, closing_parenthesis_index - 1) if index.zero?
     split
   end
 
   def slice_at(expression, index)
-    [expression.slice(0..index-1), expression[index], expression.slice(index+1..-1)]
+    [expression.slice(0..index - 1),
+     expression[index],
+     expression.slice(index + 1..-1)]
   end
-
-
 end
--- spec/expression_analyzer_spec.rb
+++ spec/expression_analyzer_spec.rb
@@ -2,62 +2,67 @@ require 'spec_helper'
 require './expression_analyzer'
 
 describe ExpressionAnalyzer do
- 
+
+  subject { described_class.new(input) }
+  let(:input) {  }
+  let(:result) { subject.call }
+
+
   context 'when pass a expression 3 * (2 + 5)' do
-    
-  let(:result) { subject.expression_dudao('3 * (2 + 5)') }
-    
+
+    let(:input) { '3 * (2 + 5)' }
+
     it 'should return 21' do
       expect(result).to eq 21 
     end
   end
 
   context 'when pass a expression (2 + 5) * 3 ' do
-    
-  let(:result) { subject.expression_dudao('(2 + 5) * 3') }
-    
+
+    let(:input) { '(2 + 5) * 3' }
+
     it 'should return 21' do
       expect(result).to eq 21 
     end
   end
 
   context 'when pass a expression (2 - 5) * 3 ' do
-    let(:result) { subject.expression_dudao('(2 - 5) * 3') }
-    
+    let(:input) { '(2 - 5) * 3' }
+
     it 'should return -9' do
       expect(result).to eq -9 
     end
   end
-  
+
   context 'when pass a expression 3 * (2 - 5)' do
-    let(:result) { subject.expression_dudao('3 * (2 - 5)') }
-    
+    let(:input) { '3 * (2 - 5)' }
+
     it 'should return -9' do
       expect(result).to eq -9 
     end
   end
 
   context 'when pass a expression 3 / (2 - 5)' do
-    let(:result) { subject.expression_dudao('3 / (2 - 5)') }
-    
+    let(:input) { '3 / (2 - 5)' }
+
     it 'should return -1' do
       expect(result).to eq -1 
     end
   end
 
   context 'when pass a expression 30 / (2 * 5)' do
-    let(:result) { subject.expression_dudao('30 / (2 * 5)') }
-    
+    let(:input) { '30 / (2 * 5)' }
+
     it 'should return 3' do
       expect(result).to eq 3 
     end
   end
 
   context 'when pass a expression [30 / (2 * 5)] + 5' do
-    let(:result) { subject.expression_dudao('[30 / (2 * 5)] + 5') }
-    
+    let(:input) { '[30 / (2 * 5)] +5' }
+
     it 'should return 8' do
       expect(result).to eq 8 
     end
   end
- end
+end
